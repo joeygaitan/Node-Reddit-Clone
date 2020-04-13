4b60cda5590aa37d6bd5153dc85d1cb5bccd85e7
joeygaitan <joeygaitan707@gmail.com>
2020-04-12 20:12:35 -0700

    pushed up to mongo setup working on figuring it out

--- server.js
+++ server.js
@@ -10,15 +10,39 @@ const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// Set db
+require('./data/reddit-db');
+
 // Routes
 
 // Start Server
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('landing-page');
+    res.render('landing-page');
   });
 
+app.get('/', (req, res) => {
+  res.render('narbar');
+});
+
+app.get('/posts/new', (req,res) => {
+
+  res.render('posts-new.handlebars');
+})
+
+require('./MVC/controllers/posts.js')(app);
+
 app.listen(3000, () => {
-  console.log('Gif Search listening on port localhost:3000!');
+  console.log('Hello friend ');
 });
